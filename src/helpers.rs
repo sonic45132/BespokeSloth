605035bfa93633527638256d05af814e3d0e1e01
Chris <git@brice.io>
2023-07-31 20:14:58 -0400

    Move functions out to helpers module

---
+++ src/helpers.rs
@@ -0,0 +1,144 @@
+use std::io;
+//use crate::State;
+use std::collections::HashMap;
+use crate::constants::Pieces;
+use crate::move_generation::Move;
+
+pub fn parse_fen(input: &str) -> [u8; 64] {
+  let hash = HashMap::from([('k',Pieces::KING), ('n', Pieces::KNIGHT), ('p', Pieces::PAWN), 
+                            ('b', Pieces::BISHOP), ('r', Pieces::ROOK), ('q', Pieces::QUEEN)]);
+
+  let mut board: [u8; 64] = [0;64];
+  let mut file: usize = 0;
+  let mut rank: usize = 7;
+
+  for c in input.chars() {
+    if c == '/' {
+      file = 0;
+      rank -= 1;
+    } else {
+      if c.is_digit(10) {
+        file += c.to_digit(10).unwrap_or(0) as usize;
+      } else {
+        let lower = c.to_lowercase().last().unwrap_or('p');
+        if c.is_uppercase() {
+          let piece = hash[&lower];
+          let color = Pieces::WHITE;
+          board[rank*8+file] = (color|piece) as u8;
+        } else {
+          let piece = hash[&lower];
+          let color = Pieces::BLACK;
+          board[rank*8+file] = (color|piece) as u8;
+        }
+        file += 1;
+      }
+    }
+  }
+  return board;
+}
+
+pub fn print_board(board: [u8; 64]) {
+  let mut file: usize = 0;
+  let mut rank: usize = 7;
+
+  loop {
+    if file == 8 {
+      print!("\n");
+      if rank == 0 {
+        print!("\n");
+        break;
+      }
+      rank -=1;
+      file = 0;
+    }
+
+
+    let current = board[rank*8+file];
+    let letter: char;
+
+    let piece = current & 0b00111;
+    let color = current & 0b11000;
+
+    match piece {
+      Pieces::PAWN => letter = 'p',
+      Pieces::ROOK => letter = 'r',
+      Pieces::KING => letter = 'k',
+      Pieces::QUEEN => letter = 'q',
+      Pieces::BISHOP => letter = 'b',
+      Pieces::KNIGHT => letter = 'n',
+      Pieces::NONE => letter = '-',
+      _ => letter = '-'
+    }
+    if color == Pieces::WHITE {
+      print!("{}", letter.to_uppercase());
+    } else {
+      print!("{}", letter);
+    }
+
+    file += 1;
+  }
+}
+
+pub fn read_line() -> String {
+  let mut input = String::new();
+  io::stdin().read_line(&mut input).expect("failed to readline");
+
+  if let Some('\n')=input.chars().next_back() {
+    input.pop();
+  }
+  if let Some('\r')=input.chars().next_back() {
+    input.pop();
+  }
+
+  input
+}
+
+pub fn to_alg(play: &Move) -> String {
+
+  let mut alg = String::new();
+
+  let alf = ['A','B','C','D','E','F','G','H'];
+
+  let s_letter = alf[(play.start%8) as usize];
+  let s_num = char::from_digit((play.start as u32/8)+1, 10).unwrap_or('0');
+
+  alg.push(s_letter);
+  alg.push(s_num);
+
+  let t_letter = alf[(play.target%8) as usize];
+  let t_num = char::from_digit((play.target as u32/8)+1, 10).unwrap_or('0');
+
+  alg.push(t_letter);
+  alg.push(t_num);
+
+  alg
+}
+
+pub fn from_alg(play: String) -> Move {
+
+  let mut s_index = 0;
+  let mut t_index = 0;
+
+  let alf = ['a','b','c','d','e','f','g','h'];
+
+  for (i, x) in play.chars().enumerate() {
+    if i < 2 {
+      if x.is_numeric() {
+        s_index += (x.to_digit(10).unwrap_or(0)-1)*8;
+      } else {
+        s_index += alf.iter().position(|&r| r == x).unwrap() as u32;
+      }
+    } else {
+      if x.is_numeric() {
+        t_index += (x.to_digit(10).unwrap_or(0)-1)*8;
+      } else {
+        t_index += alf.iter().position(|&r| r == x).unwrap() as u32;
+      }
+    }
+  }
+
+  Move {
+    start: s_index as u8,
+    target: t_index as u8
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,161 +3,22 @@
 mod evaluation;
 mod constants;
 mod move_generation;
+mod helpers;
 
-use std::collections::HashMap;
 use constants::Pieces;
 use evaluation::score_board;
 use move_generation::Move;
 use rand::Rng;
-use std::io;
 
-struct State {
-	board: [u8; 64],
-	to_move: u8,
-	moves_made: u32,
+use helpers::*;
+
+pub struct State {
+	pub board: [u8; 64],
+	pub to_move: u8,
+	pub moves_made: u32,
+	pub white_castle: u8,
+	pub black_castle: u8
 }
-
-
-
-fn parse_fen(input: &str) -> [u8; 64] {
-	let hash = HashMap::from([('k',Pieces::KING), ('n', Pieces::KNIGHT), ('p', Pieces::PAWN), 
-														('b', Pieces::BISHOP), ('r', Pieces::ROOK), ('q', Pieces::QUEEN)]);
-
-	let mut board: [u8; 64] = [0;64];
-	let mut file: usize = 0;
-	let mut rank: usize = 7;
-
-	for c in input.chars() {
-		if c == '/' {
-			file = 0;
-			rank -= 1;
-		} else {
-			if c.is_digit(10) {
-				file += c.to_digit(10).unwrap_or(0) as usize;
-			} else {
-				let lower = c.to_lowercase().last().unwrap_or('p');
-				if c.is_uppercase() {
-					let piece = hash[&lower];
-					let color = Pieces::WHITE;
-					board[rank*8+file] = (color|piece) as u8;
-				} else {
-					let piece = hash[&lower];
-					let color = Pieces::BLACK;
-					board[rank*8+file] = (color|piece) as u8;
-				}
-				file += 1;
-			}
-		}
-	}
-	return board;
-}
-
-fn print_board(board: [u8; 64]) {
-	let mut file: usize = 0;
-	let mut rank: usize = 7;
-
-	loop {
-		if file == 8 {
-			print!("\n");
-			if rank == 0 {
-				print!("\n");
-				break;
-			}
-			rank -=1;
-			file = 0;
-		}
-
-
-		let current = board[rank*8+file];
-		let letter: char;
-
-		let piece = current & 0b00111;
-		let color = current & 0b11000;
-
-		match piece {
-			Pieces::PAWN => letter = 'p',
-			Pieces::ROOK => letter = 'r',
-			Pieces::KING => letter = 'k',
-			Pieces::QUEEN => letter = 'q',
-			Pieces::BISHOP => letter = 'b',
-			Pieces::KNIGHT => letter = 'n',
-			Pieces::NONE => letter = '-',
-			_ => letter = '-'
-		}
-		if color == Pieces::WHITE {
-			print!("{}", letter.to_uppercase());
-		} else {
-			print!("{}", letter);
-		}
-
-		file += 1;
-	}
-}
-
-fn read_line() -> String {
-	let mut input = String::new();
-	io::stdin().read_line(&mut input).expect("failed to readline");
-
-	if let Some('\n')=input.chars().next_back() {
-		input.pop();
-	}
-	if let Some('\r')=input.chars().next_back() {
-		input.pop();
-	}
-
-	input
-}
-
-fn to_alg(play: &Move) -> String {
-
-	let mut alg = String::new();
-
-	let alf = ['A','B','C','D','E','F','G','H'];
-
-	let s_letter = alf[(play.start%8) as usize];
-	let s_num = char::from_digit((play.start as u32/8)+1, 10).unwrap_or('0');
-
-	alg.push(s_letter);
-	alg.push(s_num);
-
-	let t_letter = alf[(play.target%8) as usize];
-	let t_num = char::from_digit((play.target as u32/8)+1, 10).unwrap_or('0');
-
-	alg.push(t_letter);
-	alg.push(t_num);
-
-	alg
-}
-
-fn from_alg(play: String) -> Move {
-
-	let mut s_index = 0;
-	let mut t_index = 0;
-
-	let alf = ['a','b','c','d','e','f','g','h'];
-
-	for (i, x) in play.chars().enumerate() {
-		if i < 2 {
-			if x.is_numeric() {
-				s_index += (x.to_digit(10).unwrap_or(0)-1)*8;
-			} else {
-				s_index += alf.iter().position(|&r| r == x).unwrap() as u32;
-			}
-		} else {
-			if x.is_numeric() {
-				t_index += (x.to_digit(10).unwrap_or(0)-1)*8;
-			} else {
-				t_index += alf.iter().position(|&r| r == x).unwrap() as u32;
-			}
-		}
-	}
-
-	Move {
-		start: s_index as u8,
-		target: t_index as u8
-	}
-}
-
 
 fn main() {
 	let starting = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
@@ -165,7 +26,9 @@ fn main() {
 	let mut state = State {
 		board: parse_fen(starting),
 		to_move: Pieces::WHITE,
-		moves_made: 0
+		moves_made: 0,
+		white_castle: 0b11,
+		black_castle: 0b11
 	};
 
 
@@ -184,7 +47,7 @@ fn main() {
 
 		print_board(state.board);
 
-		let moves = move_generation::generate_moves(state.board, Pieces::BLACK);
+		let moves = move_generation::generate_moves(&state, Pieces::BLACK);
 
 		let picked = &moves[rng.gen_range(0..moves.len())];
 	  println!("Picked move: {}", to_alg(picked));
@@ -195,29 +58,6 @@ fn main() {
 
 	  print_board(state.board);
 	}
-
-	// if input != "uci" { panic!("Wrong start"); }
-	// println!("id name BespokeSloth");
-	// println!("id author neonstatic");
-	// println!("uciok");
-
-	// input = read_line();
-	// if input != "isready" { panic!("Not ready"); }
-	// println!("readyok");
-
-	// input = read_line();
-	// if input != "ucinewgame" { panic!("no new game"); }
-
-	// input = read_line();
-	// if !input.contains("position startpos") { panic!("bad position"); }
-	// let parts = input.split(" ");
-
-	
-
-	// while input != "quit" {
-	// 	input = read_line();
-	// 	println!("{0}", input);
-	// }
 
 	// let mut rng = rand::thread_rng();
 
