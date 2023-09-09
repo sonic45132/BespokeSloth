339c8ba6fa8a9d1d03fac12bb041e3e11cf85f7d
Chris <git@brice.io>
2023-09-09 11:57:07 -0400

    Add basic game play loop

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod zorbist;
 
 
 use constants::Pieces;
-use constants::TEntry;
+//use constants::TEntry;
 //use move_generation::*;
 //use rand::Rng;
 use search::*;
@@ -17,15 +17,37 @@ use search::*;
 use helpers::*;
 
 fn main() {
+
+	if read_line() != "start" {
+		std::process::exit(1);
+	}
+
 	let starting = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
-	let state = parse_fen(starting);
+	let mut state = parse_fen(starting);
 
+	let mut side = Pieces::WHITE;
 
-	let best_move = minimax(&state, Pieces::WHITE, 5);
+	if read_line() != "white" {
+		side = Pieces::BLACK;
+	}
+
+	if side == Pieces::WHITE {
+		let best_move = negamax(&state, side, 7);
+		println!("{:?}",to_alg(&best_move));
+		state = make_move(&state, best_move, state.to_move);
+	}
+
+	loop {
+		let other_move = from_alg(read_line());
+		state = make_move(&state, other_move, state.to_move);
+
+		let best_move = negamax(&state, side, 7);
+		println!("{:?}",to_alg(&best_move));
+		state = make_move(&state, best_move, state.to_move);
+	}
 	
-	println!("{:?}",best_move);
 
-	let mut t_table: [TEntry; 2^24];
+	//let mut t_table: [TEntry; 2^24];
 
 }
